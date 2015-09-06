97d781c53a6e948faca09abf221b5bd322a2d4f5
Selina <enigmacat@users.noreply.github.com>
2015-09-06 01:24:06 -0400

    first commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Selina Wang on 9/5/15.
+//  Copyright © 2015 #swang inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
